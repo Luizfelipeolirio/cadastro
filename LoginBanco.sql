b73a49b07e5b6ca60b528060491bb6f05b331fb3
Luizfelipeolirio <115048215+Luizfelipeolirio@users.noreply.github.com>
2023-05-23 10:09:00 -0300

    Update LoginBanco.sql

--- LoginBanco.sql
+++ LoginBanco.sql
@@ -1,3 +1,13 @@
-use login;
+CREATE DATABASE bdCadastro;
 
-select * from tbUsuario;
+USE cdCadastro;
+
+CREATE TABLE tbUsuario (
+nmUsuario varchar (20),
+dsSenha varchar (5)
+);
+
+SELECT * FROM tbUsuario;
+
+nmUsuario Luiz
+nmSenha 12345
